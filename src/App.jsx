843a02f758bffbb8fe745016a04266deb6ba8d64
Abanoub Ashraf <AbanoubAshrafFawzy@gmail.com>
2024-12-10 20:19:41 +0200

    project file upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-red-600 ">Hello Rubic's Users</h1>
+    </>
+  );
+}
+
+export default App;
